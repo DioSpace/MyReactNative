902c87459a3b86fac12dafd98903bfc0ee6c9290
hello <hello@hellodeMacBook-Pro-2.local>
2019-05-03 13:06:19 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import Home from './src/screens/Home';
+import Settings from './src/screens/Settings';
+import HomeDetail from './src/screens/HomeDetail';
+import Login from './src/screens/Login'
+
+const stackNavigationHome = createStackNavigator({ Home , HomeDetail});
+const stackNavigationSetting = createStackNavigator({ Settings},{headerMode: 'none'});
+
+const TabNavigator = createBottomTabNavigator({
+  home: stackNavigationHome,
+  settings: stackNavigationSetting,
+});
+
+const stackNavigationLogin = createStackNavigator({ Login, TabNavigator},{headerMode: 'none'});
+
+export default createAppContainer(stackNavigationLogin);
